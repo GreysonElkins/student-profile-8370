6b31cc040d89da736c04dab1ed55c623e9b586d4
Greyson Elkins <greysonelkins@gmail.com>
2021-10-30 16:56:57 -0600

    Toggle grades on PlusMinus click

--- src/components/Student/StudentProfile/index.tsx
+++ src/components/Student/StudentProfile/index.tsx
@@ -1,14 +1,22 @@
+import { useState } from 'react'
 import type Student from 'types/Student'
 import PlusMinus from 'components/style/Button/PlusMinus'
 
 import './StudentProfile.scss'
+import StudentGrades from '../StudentGrades'
 
 type Props = {
   student: Student
 }
 
-const StudentProfile: React.FC<Props> = ({ student: { city, company, email, firstName, grades, id, lastName, pic, skill } }) => {
+const StudentProfile: React.FC<Props> = ({ student }) => {
+  const { company, email, firstName, grades, id, lastName, pic, skill } = student
+  const [showGrades, setShowGrades] = useState<boolean>(false)
+
   const getAverage = () => {
+    // I could see this function being used in other parts of the app, 
+    // in which case I would store it in `src/scripts`
+    // or place it as a method on a Student class (which I'd use in place of the type)
     if (!grades || grades.length === 0) return `No grades have been taken for this student`
     const avg = grades.reduce((avg, grade, i) => {
       const num = Number(grade)
@@ -26,7 +34,7 @@ const StudentProfile: React.FC<Props> = ({ student: { city, company, email, firs
     <div className="StudentProfile">
       {pic && <img src={pic} alt={`${firstName ? firstName : 'unnamed student'}'s picture'`} />}
       <div className="profileExtendButton">
-        <PlusMinus onClick={() => console.log('hi')}/>
+        <PlusMinus onClick={(isOpen) => setShowGrades(isOpen)}/>
       </div>
       <div className="studentInfo">
         {(firstName || lastName) && (
@@ -38,6 +46,7 @@ const StudentProfile: React.FC<Props> = ({ student: { city, company, email, firs
         {company && <div>Company: {company}</div>}
         {skill && <div>Skill: {skill}</div>}
         <div>Average: {getAverage()}</div>
+        {showGrades && <StudentGrades student={student} />}
       </div>
     </div>
   )
