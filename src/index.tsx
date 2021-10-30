dae7f0a0d3f9df00e1ee6079886895beb5fcc68c
Greyson Elkins <greysonelkins@gmail.com>
2021-10-30 15:03:06 -0600

    Add toast

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+toast.configure({
+  position: 'bottom-center',
+  autoClose: 4000,
+})
 
 import App from 'components/App'
 
