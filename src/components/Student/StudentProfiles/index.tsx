11d57d8513e77336712ff07112802d8ddaa69518
Greyson Elkins <greysonelkins@gmail.com>
2021-10-30 17:56:14 -0600

    Add no students state, add UnderlinedTextField

--- src/components/Student/StudentProfiles/index.tsx
+++ src/components/Student/StudentProfiles/index.tsx
@@ -1,24 +1,39 @@
 import { useState, useEffect } from 'react'
 import { getStudents } from 'scripts/API'
 
-import type Student from 'types/Student'
+import Student from 'types/Student'
 import StudentProfile from '../StudentProfile'
+import UnderlinedTextField from 'components/style/Field/UnderlinedTextField'
 import './StudentProfiles.scss'
 
 const StudentProfiles: React.FC = () => {
   const [students, setStudents] = useState<Student[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
   
   useEffect(() => {
    getStudents().then(result => {
-    if (result?.students) setStudents(result.students)
+    if (result?.students) {
+      const parsedStudents = result.students.map(student => new Student(student))
+      setStudents(parsedStudents)
+    }
+    setLoading(false)
    })
   }, [])
 
   const printStudentProfiles = () =>
     students.map((student, i) => <StudentProfile student={student} key={`${student}-${i}`} />)
   
+  if (!loading && students.length === 0) return <div className="noStudents">No students were found</div>
 
-  return <div className="StudentProfiles">{printStudentProfiles()}</div>
+  return (
+    <div className="StudentProfiles">
+      <UnderlinedTextField
+        placeholder="Search by student, or tags (separate queries by comma)"
+        onChange={(e) => console.log(e)}
+      />
+      {printStudentProfiles()}
+    </div>
+  )
 }
 
 export default StudentProfiles
