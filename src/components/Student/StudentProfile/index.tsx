d90e7f99a2d8e2db9a55ad77cc9f57a6e2063416
Greyson Elkins <greysonelkins@gmail.com>
2021-10-30 17:55:39 -0600

    Remove average, add UnderlinedTextField

--- src/components/Student/StudentProfile/index.tsx
+++ src/components/Student/StudentProfile/index.tsx
@@ -2,39 +2,23 @@ import { useState } from 'react'
 import type Student from 'types/Student'
 import PlusMinus from 'components/style/Button/PlusMinus'
 
-import './StudentProfile.scss'
 import StudentGrades from '../StudentGrades'
+import UnderlinedTextField from 'components/style/Field/UnderlinedTextField'
+import './StudentProfile.scss'
 
 type Props = {
   student: Student
 }
 
 const StudentProfile: React.FC<Props> = ({ student }) => {
-  const { company, email, firstName, grades, id, lastName, pic, skill } = student
+  const { average, company, email, firstName, lastName, pic, skill } = student
   const [showGrades, setShowGrades] = useState<boolean>(false)
-
-  const getAverage = () => {
-    // I could see this function being used in other parts of the app, 
-    // in which case I would store it in `src/scripts`
-    // or place it as a method on a Student class (which I'd use in place of the type)
-    if (!grades || grades.length === 0) return `No grades have been taken for this student`
-    const avg = grades.reduce((avg, grade, i) => {
-      const num = Number(grade)
-      if (isNaN(num)) {
-        console.warn(`Student ${id}'s grade at index ${i} is not a number'`)
-      } else {
-        avg += num
-      } 
-      return avg
-    }, 0) / grades.length
-    return `${avg}%`
-  }
 
   return (
     <div className="StudentProfile">
       {pic && <img src={pic} alt={`${firstName ? firstName : 'unnamed student'}'s picture'`} />}
       <div className="profileExtendButton">
-        <PlusMinus onClick={(isOpen) => setShowGrades(isOpen)}/>
+        <PlusMinus onClick={(isOpen) => setShowGrades(isOpen)} />
       </div>
       <div className="studentInfo">
         {(firstName || lastName) && (
@@ -45,7 +29,12 @@ const StudentProfile: React.FC<Props> = ({ student }) => {
         {email && <div>Email: {email}</div>}
         {company && <div>Company: {company}</div>}
         {skill && <div>Skill: {skill}</div>}
-        <div>Average: {getAverage()}</div>
+        <div>Average: {average}%</div>
+        <UnderlinedTextField
+          placeholder="Add a tag"
+          onSubmit={() => console.log('success')}
+          className="tagInput"
+        />
         {showGrades && <StudentGrades student={student} />}
       </div>
     </div>
