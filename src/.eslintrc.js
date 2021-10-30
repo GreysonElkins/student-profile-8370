79740d8a2323a27b6c5cb169f5634f83b1a18b05
Greyson Elkins <greysonelkins@gmail.com>
2021-10-30 17:54:30 -0600

    Add note on lint inclusion

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // since I've updated node I've had a problem not using commas in type declarations 
+  // - including this here as a quick fix until I have time to figure out the better fix
+
   extends: [
     'plugin:@typescript-eslint/recommended',
   ],
