5f8ddda31a9cf87c9010579ae494a7a83e3761af
Greyson Elkins <greysonelkins@gmail.com>
2021-10-30 18:35:23 -0600

    Initial commit

---
+++ src/scripts/search/index.ts
@@ -0,0 +1,19 @@
+export const splitQuery = (query: string) => {
+  let queries = [] as string[]
+  if (query.includes(', ')) {
+    queries = query.split(', ') 
+  } else if (query.includes(' ')) {
+    queries = query.split(' ')
+  } else {
+    queries = [query]
+  }
+  checkForHangingComma(queries)
+  return queries
+}
+
+const checkForHangingComma = (queries: string[]) => {
+  const i = queries.length - 1
+  if (queries[i][queries[i].length - 1] === ',') {
+    queries[i] = queries[i].slice(0, queries[i].length - 1)
+  }
+}
