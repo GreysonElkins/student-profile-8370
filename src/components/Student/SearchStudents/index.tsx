3cf6f6917bd298eaed8a4da46b401f3b0d878e54
Greyson Elkins <greysonelkins@gmail.com>
2021-10-31 14:18:14 -0600

    Comment on SearchEngine strategy

--- src/components/Student/SearchStudents/index.tsx
+++ src/components/Student/SearchStudents/index.tsx
@@ -27,6 +27,8 @@ const SearchStudents: React.FC = () => {
     <SingleWrapper>
       <SearchEngine
           data={students}
+          // we'll pass students through the search engine before they hit profiles
+          // in "data" => filtered by search => out "data" on line 35
           matchRule="hasMatchingProps"
           placeholder="Search by student and/or tags"
       > 
