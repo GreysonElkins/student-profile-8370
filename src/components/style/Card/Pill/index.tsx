f9e2f7c67a033b54bca5da57a3cc14e60c79c64b
Greyson Elkins <greysonelkins@gmail.com>
2021-10-31 13:30:03 -0600

    Default to non-button

--- src/components/style/Card/Pill/index.tsx
+++ src/components/style/Card/Pill/index.tsx
@@ -1,8 +1,16 @@
+import { MouseEvent } from 'react'
 import './Pill.scss'
 
-const Pill: React.FC<{ onClick?: void }> = ({ onClick, children }) => {
-  if (onClick) return <button className="Pill button">{children}</button>
-  return <button className="Pill button">{children}</button>
+const Pill: React.FC<{
+  onClick?: (event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => void
+}> = ({ onClick, children }) => {
+  if (onClick)
+    return (
+      <button onClick={(event) => onClick(event)} className="Pill button">
+        {children}
+      </button>
+    )
+  return <div className="Pill">{children}</div>
 }
 
 export default Pill
